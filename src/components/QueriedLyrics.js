e3396eafa3a05c32dc0c13af3440d83f8c31229c
Shayna Kothari <shayna.kothari@gmail.com>
2020-12-29 00:03:15 -0800

    added number of usages, fixed modal spacing on mobile

--- src/components/QueriedLyrics.js
+++ src/components/QueriedLyrics.js
@@ -10,28 +10,62 @@ type QueriedLyricsProps = {
   query: string,
 };
 
-class QueriedLyrics extends React.Component<QueriedLyricsProps> {
+type QueriedLyricsState = {
+  occurrences: number,
+};
+
+class QueriedLyrics extends React.Component<
+  QueriedLyricsProps,
+  QueriedLyricsState
+> {
+  constructor(props: QueriedLyricsProps) {
+    super(props);
+    this.state = {
+      occurrences: 0,
+    };
+  }
+
+  componentDidMount(): any {
+    this.setState({
+      occurrences: document.querySelectorAll(".query").length,
+    });
+  }
+
+  componentDidUpdate(): any {
+    const occurrences = document.querySelectorAll(".query").length;
+    if (this.state.occurrences != occurrences) {
+      this.setState({
+        occurrences: occurrences,
+      });
+    }
+  }
+
   render(): any {
     return (
-      <div className={mobile ? "QueriedLyrics-mobile" : "QueriedLyrics"}>
-        {Object.keys(lyricsJSON).map((album) =>
-          Object.keys(lyricsJSON[album]).map((song) =>
-            lyricsJSON[album][song].map((songLyric) => {
-              if (containsQuery(songLyric.lyric, this.props.query) >= 0) {
-                return (
-                  <SongLyric
-                    album={album}
-                    song={song}
-                    lyric={songLyric.lyric}
-                    next={songLyric.next}
-                    prev={songLyric.prev}
-                    query={this.props.query}
-                  />
-                );
-              }
-            })
-          )
-        )}
+      <div>
+        <div className={mobile ? "QueriedLyrics-mobile" : "QueriedLyrics"}>
+          {Object.keys(lyricsJSON).map((album) =>
+            Object.keys(lyricsJSON[album]).map((song) =>
+              lyricsJSON[album][song].map((songLyric) => {
+                if (containsQuery(songLyric.lyric, this.props.query) >= 0) {
+                  return (
+                    <SongLyric
+                      album={album}
+                      song={song}
+                      lyric={songLyric.lyric}
+                      next={songLyric.next}
+                      prev={songLyric.prev}
+                      query={this.props.query}
+                    />
+                  );
+                }
+              })
+            )
+          )}
+        </div>
+        <div className={mobile ? "totalResults-mobile" : "totalResults"}>
+          Total usages found: {this.state.occurrences}
+        </div>
       </div>
     );
   }
